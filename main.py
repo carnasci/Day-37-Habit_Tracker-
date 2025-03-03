544a69dda455f26db83b413e496e53975b319848
carnasci <cen.master24@gmail.com>
2025-03-03 14:45:47 -0600

    Day 37 - created and configured a habit tracking pixel graph using another post request to Pixela

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import requests
-from credentials import pixela_params
+from credentials import pixela_params, pixela_token, username
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
-response = requests.post(url=pixela_endpoint, json=pixela_params)
+#response = requests.post(url=pixela_endpoint, json=pixela_params)
+#print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+
+graph_config = {
+    "id" : "graph1",
+    "name" : "Hours Coding",
+    "unit" : "hours",
+    "type" : "float",
+    "color" : "ichou"
+}
+
+headers = {
+    "X-USER-TOKEN" : pixela_token,
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
