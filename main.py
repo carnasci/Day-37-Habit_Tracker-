ca000c74a07820849c5ef092016e36d8bf25819c
carnasci <cen.master24@gmail.com>
2025-03-03 15:23:47 -0600

    Day 37 - was able to automate the date input using datetime and also added more inputs for the beginning of February to fill out my tracker graph with more data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 from credentials import pixela_params, pixela_token, username
 
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -23,12 +24,14 @@ headers = {
 #response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 #print(response.text)
 
-add_pixel_endpoint = f"https://pixe.la/v1/users/{username}/graphs/graph1"
-date = "20250303"
+add_pixel_endpoint = f"{pixela_endpoint}/{username}/graphs/graph1"
+today = datetime(year=2025, month=2, day=1)
+date = today.strftime("%Y%m%d")
+print(date)
 add_pixel_params = {
     "date" : date,
-    "quantity" : "3.5",
+    "quantity" : "1.5",
 }
 
-response = requests.post(url=add_pixel_endpoint, json=add_pixel_params, headers=headers)
-print(response.text)
+# response = requests.post(url=add_pixel_endpoint, json=add_pixel_params, headers=headers)
+# print(response.text)
