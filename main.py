78f52a454886e7aa1542c99387399e27e5ff5d0b
carnasci <cen.master24@gmail.com>
2025-03-03 15:42:46 -0600

    Day 37 - practiced using all the request methods(GET, POST, PUT, DELETE) project finished.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from credentials import pixela_params, pixela_token, username
 
 pixela_endpoint = "https://pixe.la/v1/users"
+graph = "graph1"
 
 #response = requests.post(url=pixela_endpoint, json=pixela_params)
 #print(response.text)
@@ -24,14 +25,26 @@ headers = {
 #response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 #print(response.text)
 
-add_pixel_endpoint = f"{pixela_endpoint}/{username}/graphs/graph1"
-today = datetime(year=2025, month=2, day=1)
+add_pixel_endpoint = f"{pixela_endpoint}/{username}/graphs/{graph}"
+today = datetime.now()
 date = today.strftime("%Y%m%d")
 print(date)
 add_pixel_params = {
     "date" : date,
-    "quantity" : "1.5",
+    "quantity" : "3.75",
 }
 
-# response = requests.post(url=add_pixel_endpoint, json=add_pixel_params, headers=headers)
+response = requests.post(url=add_pixel_endpoint, json=add_pixel_params, headers=headers)
+print(response.text)
+
+put_pixel_endpoint = f"{pixela_endpoint}/{username}/graphs/{graph}/{date}"
+put_params = {
+    "quantity" : "3.75"
+}
+# response = requests.put(url=put_pixel_endpoint, json=put_params, headers=headers)
+# print(response.text)
+
+delete_pixel_endpoint = f"{pixela_endpoint}/{username}/graphs/{graph}/{date}"
+
+# response = requests.delete(url=delete_pixel_endpoint, headers=headers)
 # print(response.text)
