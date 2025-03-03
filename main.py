d2c3fb1c5ea7dd8877f70bddd54faaa646396cff
carnasci <cen.master24@gmail.com>
2025-03-03 14:21:59 -0600

    Day 37 - used post method to create a user profile for the Pixela API

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import requests
+from credentials import pixela_params
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+response = requests.post(url=pixela_endpoint, json=pixela_params)
+print(response.text)
